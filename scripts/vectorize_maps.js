a9be14a21e881e645e495d4f026d485ca7a24b03
raikutro <raikutro@gmail.com>
2022-06-02 18:00:06 -0700

    Added more minimal elements

--- scripts/vectorize_maps.js
+++ scripts/vectorize_maps.js
@@ -5,6 +5,7 @@ const util = require('util');
 const path = require('path');
 const TPMI = require('../tpmi-toolkit');
 const cliProgress = require('cli-progress');
+const colors = require('ansi-colors');
 
 const writeFile = util.promisify(fs.writeFile);
 const readdir = util.promisify(fs.readdir);
@@ -20,8 +21,10 @@ const seed = cyrb128(SETTINGS.RANDOM_SEED);
 const random = sfc32(...seed);
 
 class Packer {
-	constructor({dimensions, pack}) {
-		this.dimensions = dimensions;
+	constructor({dimensionsPerElement, numberOfElements, pack}) {
+		this.dimensionsPerElement = dimensionsPerElement;
+		this.numberOfElements = numberOfElements;
+		this.dimensions = this.numberOfElements * this.dimensionsPerElement;
 		this.pack = (vMap) => pack(this, vMap);
 	}
 }
@@ -29,74 +32,144 @@ class Packer {
 const ELEMENT_PACKERS = {
 	OUTER_WALL: new Packer({
 		// 50 Wall Nodes: 50x(x, y)
-		dimensions: 50 * 2,
+		dimensionsPerElement: 2,
+		numberOfElements: 50,
 		pack: (self, vectorMap) => {
-			let outerWallFlatVector = vectorMap.elements.outerWall.toPoints();
-			outerWallFlatVector = adjustPointsToTargetAmount(outerWallFlatVector, self.dimensions);
-			outerWallFlatVector = outerWallFlatVector.map(p => [p.x / vectorMap.width, p.y / vectorMap.height]).flat();
-			return outerWallFlatVector;
+			let outerWallVector = vectorMap.elements.outerWall.toPoints();
+			outerWallVector = adjustPointsToTargetAmount(outerWallVector, self.dimensions);
+			outerWallVector = outerWallVector.map(p => [p.x / vectorMap.width, p.y / vectorMap.height]).flat();
+			return outerWallVector;
 		}
 	}),
 	ISLANDS: new Packer({
 		// 8, 16 Island Node + 1 Exist Param per island: 8x(exists, 16x(x, y))
-		dimensions: 8 * ((16 * 2) + 1),
+		dimensionsPerElement: (16 * 2) + 1,
+		numberOfElements: 8,
 		pack: (self, vectorMap) => {
-			
+			const centerPoint = TPMI.Flatten.point(vectorMap.width / 2, vectorMap.height / 2);
+			// Sort by closest distance to center
+			let islandPoints = vectorMap.elements.islands.sort((a, b) => {
+				return a.shape.distanceTo(centerPoint)[0] - b.shape.distanceTo(centerPoint)[0]
+			});
+			islandPoints = islandPoints.map(i => i.toPoints());
+			islandPoints = islandPoints.map(island => adjustPointsToTargetAmount(island, 16));
+			islandPoints = markElementsExistence(
+				islandPoints.map(island => island.map(p => [p.x / vectorMap.width, p.y / vectorMap.height]).flat()),
+				self.numberOfElements,
+				Array(16).fill(0).map(p => [centerPoint.x, centerPoint.y]).flat()
+			).flat();
+			return islandPoints;
 		}
 	}),
 	BOOSTS: new Packer({
-		// 8 Boost Nodes + 1 Team Param + 1 Exist Param: 8x(exists, x, y, team)
-		dimensions: 8 * (2 + 1 + 1),
+		// 16 Boost Nodes + 1 Team Param + 1 Exist Param: 16x(exists, x, y, team)
+		dimensionsPerElement: 2 + 1 + 1,
+		numberOfElements: 16,
 		pack: (self, vectorMap) => {
-			
+			const centerPoint = TPMI.Flatten.point(vectorMap.width / 2, vectorMap.height / 2);
+			// Sort by closest distance to center
+			let boostPoints = vectorMap.elements.boosts.sort((a, b) => {
+				return a.shape.distanceTo(centerPoint)[0] - b.shape.distanceTo(centerPoint)[0]
+			});
+			boostPoints = boostPoints.map(b => [b.shape.x / vectorMap.width, b.shape.y / vectorMap.height, teamToNumber(b.team)]);
+			boostPoints = markElementsExistence(boostPoints, self.numberOfElements, [centerPoint.x, centerPoint.y, 0.5]).flat();
+			return boostPoints;
 		}
 	}),
 	BOMBS: new Packer({
 		// 8 Bomb Nodes + 1 Exist Param: 8x(exists, x, y)
-		dimensions: 8 * (2 + 1),
+		dimensionsPerElement: 2 + 1,
+		numberOfElements: 8,
 		pack: (self, vectorMap) => {
-			
+			const centerPoint = TPMI.Flatten.point(vectorMap.width / 2, vectorMap.height / 2);
+			// Sort by closest distance to center
+			let bombPoints = vectorMap.elements.bombs.sort((a, b) => {
+				return a.shape.distanceTo(centerPoint)[0] - b.shape.distanceTo(centerPoint)[0]
+			});
+			bombPoints = bombPoints.map(b => [b.shape.x / vectorMap.width, b.shape.y / vectorMap.height]);
+			bombPoints = markElementsExistence(bombPoints, self.numberOfElements, [centerPoint.x, centerPoint.y]).flat();
+			return bombPoints;
 		}
 	}),
 	SPIKES: new Packer({
 		// 16 Spike Nodes + 1 Exist Param: 16x(exists, x, y)
-		dimensions: 16 * (2 + 1),
+		dimensionsPerElement: 2 + 1,
+		numberOfElements: 8,
 		pack: (self, vectorMap) => {
-			
+			const centerPoint = TPMI.Flatten.point(vectorMap.width / 2, vectorMap.height / 2);
+			// Sort by closest distance to center
+			let spikePoints = vectorMap.elements.spikes.sort((a, b) => {
+				return a.shape.distanceTo(centerPoint)[0] - b.shape.distanceTo(centerPoint)[0]
+			});
+			spikePoints = spikePoints.map(b => [b.shape.x / vectorMap.width, b.shape.y / vectorMap.height]);
+			spikePoints = markElementsExistence(spikePoints, self.numberOfElements, [centerPoint.x, centerPoint.y]).flat();
+			return spikePoints;
 		}
 	}),
 	PORTALS: new Packer({
 		// 8 Portal Nodes + 1 Exist Param: 8x(exists, x, y)
-		dimensions: 8 * (2 + 1),
+		dimensionsPerElement: 2 + 1,
+		numberOfElements: 8,
 		pack: (self, vectorMap) => {
-			
+			const centerPoint = TPMI.Flatten.point(vectorMap.width / 2, vectorMap.height / 2);
+			// Sort by closest distance to center
+			let portalPoints = vectorMap.elements.portals.sort((a, b) => {
+				return a.shape.distanceTo(centerPoint)[0] - b.shape.distanceTo(centerPoint)[0]
+			});
+			portalPoints = portalPoints.map(b => [b.shape.x / vectorMap.width, b.shape.y / vectorMap.height]);
+			portalPoints = markElementsExistence(portalPoints, self.numberOfElements, [centerPoint.x, centerPoint.y]).flat();
+			return portalPoints;
 		}
 	}),
 	GATES: new Packer({
 		// 8, 16 Gate Nodes + 1 Exist Param per island: 8x(exists, 16x(x, y))
-		dimensions: 8 * ((16 * 2) + 1),
+		dimensionsPerElement: (16 * 2) + 1,
+		numberOfElements: 8,
 		pack: (self, vectorMap) => {
-			
+			const centerPoint = TPMI.Flatten.point(vectorMap.width / 2, vectorMap.height / 2);
+			// Sort by closest distance to center
+			let gatePoints = vectorMap.elements.gates.sort((a, b) => {
+				return a.shape.distanceTo(centerPoint)[0] - b.shape.distanceTo(centerPoint)[0]
+			});
+			gatePoints = gatePoints.map(i => i.toPoints());
+			gatePoints = gatePoints.map(gate => adjustPointsToTargetAmount(gate, 16));
+			gatePoints = markElementsExistence(
+				gatePoints.map(gate => gate.map(p => [p.x / vectorMap.width, p.y / vectorMap.height]).flat()),
+				self.numberOfElements,
+				Array(16).fill(0).map(p => [centerPoint.x, centerPoint.y]).flat()
+			).flat();
+			return gatePoints;
 		}
 	}),
 	FLAGS: new Packer({
 		// 1 Flag Node + Color
-		dimensions: 2 + 1,
+		dimensionsPerElement: 2 + 1,
+		numberOfElements: 1,
 		pack: (self, vectorMap) => {
-			
+			const flag = TPMI.VectorUtilities.getFlagPair(vectorMap.elements.flags)[0];
+			return [flag.x, flag.y, teamToNumber(flag.team)];
 		}
 	}),
 	POWERUPS: new Packer({
 		// 3 Powerup Nodes + Exists
-		dimensions: 3 * (2 + 1),
+		dimensionsPerElement: 2 + 1,
+		numberOfElements: 3,
 		pack: (self, vectorMap) => {
-			
+			const centerPoint = TPMI.Flatten.point(vectorMap.width / 2, vectorMap.height / 2);
+			// Sort by closest distance to center
+			let powerupPoints = vectorMap.elements.powerups.sort((a, b) => {
+				return a.shape.distanceTo(centerPoint)[0] - b.shape.distanceTo(centerPoint)[0]
+			});
+			powerupPoints = powerupPoints.map(b => [b.shape.x / vectorMap.width, b.shape.y / vectorMap.height]);
+			powerupPoints = markElementsExistence(powerupPoints, self.numberOfElements, [centerPoint.x, centerPoint.y]).flat();
+			return powerupPoints;
 		}
 	})
 };
 
 const ELEMENT_CURATIONS = {
-	WALLS: ["OUTER_WALL"]
+	WALLS: ["OUTER_WALL", "ISLANDS"],
+	MINIMAL: ["OUTER_WALL", "ISLANDS", "BOOSTS", "BOMBS", "SPIKES", "PORTALS", "GATES", "FLAGS", "POWERUPS"]
 };
 
 (async () => {
@@ -124,7 +197,9 @@ const ELEMENT_CURATIONS = {
 
 	logger("Generating map vectors...");
 
-	const progressBar = new cliProgress.SingleBar();
+	const progressBar = new cliProgress.SingleBar({
+		format: 'Generating Vectors |' + colors.cyan('{bar}') + '| {percentage}% | {value}/{total} Maps | Processing Map: {mapPath}',
+	}, cliProgress.Presets.shades_classic);
 	const curationElements = ELEMENT_CURATIONS[elementsTemplate];
 	const totalDimensions = curationElements.reduce((acc, val) => acc + ELEMENT_PACKERS[val].dimensions, 0);
 	const dimensionRanges = curationElements.reduce((acc, val) => ([
@@ -138,16 +213,27 @@ const ELEMENT_CURATIONS = {
 		"data": [
 	`);
 
-	progressBar.start(mapFilePaths.length, 1);
+	let completedMaps = 0;
+	progressBar.start(mapFilePaths.length, 0);
 	for (let i = 0; i < mapFilePaths.length; i++) {
+		progressBar.increment({
+			mapPath: path.basename(mapFilePaths[i])
+		});
+
 		try {
 			const tileMap = await TPMI.MapUtilities.fileToTileMap(mapFilePaths[i]);
+			
 			const vectorMap = TPMI.Vectorizer.createVectorMapFromTileMap(tileMap);
-			const flags = TPMI.VectorUtilities.getFlagPair(vectorMap.elements.flags);
+			if(vectorMap.width < 10 || vectorMap.height < 10) continue;
 
+			const flags = TPMI.VectorUtilities.getFlagPair(vectorMap.elements.flags);
 			if(flags[0] === null) continue;
 			if(flags[0].team === TPMI.CONSTANTS.TEAMS.NONE && gameMode !== "NF") continue;
 			if(flags[0].team === TPMI.CONSTANTS.TEAMS.RED && gameMode !== "CTF") continue;
+
+			const symmetry = TPMI.Analyzer.detectVectorMapSymmetry.closest(vectorMap);
+
+			console.log(symmetry);
 
 			let mapVector = [];
 
@@ -161,11 +247,10 @@ const ELEMENT_CURATIONS = {
 
 			let dataString = JSON.stringify(mapVector);
 			fileStream.write(`${dataString}${i !== mapFilePaths.length - 1 ? "," : ""}\n`);
+			completedMaps++;
 		} catch (e) {
 			logger(`An error occurred while processing map "${mapFilePaths[i]}":`, e);
 		}
-
-		progressBar.increment();
 
 		// Quick wait
 		await new Promise(res => setTimeout(res, 0));
@@ -175,7 +260,7 @@ const ELEMENT_CURATIONS = {
 
 	fileStream.end("]\n}");
 
-	logger(`Finished generating dataspace ${spaceName}.json`);
+	logger(`Finished generating dataspace ${spaceName}.json with ${completedMaps}/${mapFilePaths.length} maps`);
 })();
 
 function adjustPointsToTargetAmount(points, targetPoints) {
@@ -209,6 +294,46 @@ function adjustPointsToTargetAmount(points, targetPoints) {
 	}
 
 	return points;
+}
+
+function markElementsExistence(elements, targetElementAmount, defaultElement=["No Default Element"]) {
+	let existenceMarked = elements.map(e => [1].concat(e));
+	defaultElement = [0].concat(defaultElement);
+
+	if(targetElementAmount > existenceMarked.length) {
+		existenceMarked = existenceMarked.concat(
+			duplicateArrayElementsToAmount(existenceMarked, targetElementAmount, defaultElement, elem => {
+				elem[0] = 0;
+				return elem;
+			})
+		);
+	} else if(targetElementAmount < existenceMarked.length) {
+		existenceMarked = existenceMarked.slice(0, targetElementAmount);
+	}
+
+	return existenceMarked;
+}
+
+function duplicateArrayElementsToAmount(array, targetAmount, defaultElement, newElementMapFunc) {
+	if(array.length === 0) return Array(targetAmount).fill(0).map(e => Array.from(defaultElement));
+	const newArray = [];
+
+	for (let i = 0; i < targetAmount; i++) {
+		const sourceArrayIndex = i % array.length;
+
+		let newElement = Array.from(array[sourceArrayIndex]);
+		
+		if(newElementMapFunc && i > array.length - 1) newElement = newElementMapFunc(newElement);
+		newArray.push(newElement);
+	}
+
+	return newArray;
+}
+
+function teamToNumber(team) {
+	if(team === TPMI.CONSTANTS.TEAMS.RED) return 0;
+	if(team === TPMI.CONSTANTS.TEAMS.BLUE) return 1;
+	return 0.5;
 }
 
 function triangleArea(p1, p2, p3) {
